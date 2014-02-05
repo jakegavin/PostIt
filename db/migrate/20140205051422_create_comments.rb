71752f9b0d27e815b165058646f9fe3b98a55a1e
Jake Gavin <jakegavin@gmail.com>
2014-02-04 23:10:03 -0800

    Initial commit

---
+++ db/migrate/20140205051422_create_comments.rb
@@ -0,0 +1,9 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.text :body
+      t.belongs_to :user
+      t.belongs_to :post
+    end
+  end
+end
