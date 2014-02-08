845f5e4a35764f28a9d3280ecb5f898c3d1e23fd
Jake Gavin <jakegavin@gmail.com>
2014-02-07 23:06:08 -0800

    Add posts and commments routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
-  resources :posts, except: [:destroy]
+  resources :posts, except: [:destroy] do
+    resources :comments, only: [:create, :show]
 end
