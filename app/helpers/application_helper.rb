31914387d9dbbd57050edca094983ca990c7d3e8
Jake Gavin <jakegavin@gmail.com>
2014-02-11 19:00:48 -0800

    Add date and url helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def fix_url(str)
+    str.start_with?("http://") ? str : "http://#{str}"
+  end
+
+  def fix_date(date)
+    if date.nil?
+      nil
+    else
+      date.strftime("%m/%d/%Y at %l:%M %p")
+    end
+  end
 end
