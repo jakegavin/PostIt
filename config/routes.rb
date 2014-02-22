f3bc9c1ad6e0d4e3a4a2435776c72e215cf875e8
Jake Gavin <jakegavin@gmail.com>
2014-02-21 16:32:21 -0800

    Adding Vote model - raises runtime error

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,15 @@ PostitTemplate::Application.routes.draw do
 
 
   resources :posts, except: [:destroy] do
-    resources :comments, only: [:create]
+    member do
+      post 'vote'
+    end
+
+    resources :comments, only: [:create] do
+      member do
+        post 'vote'
+      end
+    end
   end
 
   resources :categories, except: [:destroy]
